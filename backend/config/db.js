008099e58584d42a22f200a65d2749d09ef95260
Bhanu1429 <135256216+Bhanu1429@users.noreply.github.com>
2025-09-16 01:21:45 +0530

    Delete backend directory

--- backend/config/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-  // This line now correctly uses the MONGO_URI from your .env file.
-  await mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => console.log("DB Connected"));
-};
