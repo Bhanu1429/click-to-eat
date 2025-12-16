38d183253563507e478fe1dbd23ffd268d2c28c2
Bhanu1429 <135256216+Bhanu1429@users.noreply.github.com>
2025-12-16 19:10:54 +0530

    Add files via upload

---
+++ frontend/src/pages/Admin/Admin.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState, useContext } from 'react';
+import './Admin.css';
+import axios from 'axios';
+import { StoreContext } from '../../context/StoreContext';
+import { assets } from '../../assets/assets';
+
+const Admin = () => {
+  const { url } = useContext(StoreContext);
+  const [orders, setOrders] = useState([]);
+
+  const fetchAllOrders = async () => {
+    const response = await axios.get(url + "/api/order/list");
+    if (response.data.success) {
+      setOrders(response.data.data);
+    } else {
+      alert("Error fetching orders");
+    }
+  };
+
+  const statusHandler = async (event, orderId) => {
+    const response = await axios.post(url + "/api/order/status", {
+      orderId,
+      status: event.target.value
+    });
+    if (response.data.success) {
+      await fetchAllOrders();
+    }
+  };
+
+  useEffect(() => {
+    fetchAllOrders();
+  }, []);
+
+  return (
+    <div className='admin-orders'>
+      <h3>Order Management</h3>
+      <div className="admin-order-list">
+        {orders.map((order, index) => (
+          <div key={index} className="admin-order-item">
+            <img src={assets.parcel_icon} alt="" />
+            <div>
+              <p className="admin-order-item-food">
+                {order.items.map((item, index) => {
+                  if (index === order.items.length - 1) {
+                    return item.name + " x " + item.quantity;
+                  } else {
+                    return item.name + " x " + item.quantity + ", ";
+                  }
+                })}
+              </p>
+              <p className="admin-order-item-name">{order.address.firstName + " " + order.address.lastName}</p>
+              <div className="admin-order-item-address">
+                <p>{order.address.street + ","}</p>
+                <p>{order.address.city + ", " + order.address.state + ", " + order.address.country + ", " + order.address.zipcode}</p>
+              </div>
+              <p className="admin-order-item-phone">{order.address.phone}</p>
+            </div>
+            <p>Items: {order.items.length}</p>
+            <p>${order.amount}</p>
+            <select onChange={(event) => statusHandler(event, order._id)} value={order.status}>
+              <option value="Food Processing">Food Processing</option>
+              <option value="Out for delivery">Out for delivery</option>
+              <option value="Delivered">Delivered</option>
+              <option value="Cancelled">Cancelled</option>
+            </select>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Admin;
